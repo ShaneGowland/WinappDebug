c50d8c7daa7e1df8341a424f8ab59bb29cf5efdc
Shane Gowland <shane@gowland.me>
2013-04-02 17:46:27 +1030

    0.3

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WinappDebug")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Shane Gowland")> 
+<Assembly: AssemblyProduct("WinappDebug")> 
+<Assembly: AssemblyCopyright("Copyright ©  2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6caf7737-f9e8-4c49-a690-08178198c7de")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("0.0.0.2")> 
+<Assembly: AssemblyFileVersion("0.0.0.2")> 
